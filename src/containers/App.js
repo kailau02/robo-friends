285f7074a38378930888a01a0fef5da0a5312407
Kai Lau <kailau02@gmail.com>
2021-05-24 21:13:42 -0700

    updated files

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 class App extends Component {
     constructor(state){
@@ -44,7 +45,9 @@ class App extends Component {
                     <SearchBox searchChanged={this.onSearchChange} searchPressed={this.onSearch} />
                     <br />
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
         )
